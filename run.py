b19c3dc2a4dd6ea8178dc6a91c056c1a811f297a
al-ell <>
2023-11-12 08:14:37 +0000

    updates surplus worksheet

--- run.py
+++ run.py
@@ -77,6 +77,16 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def update_surplus_worksheet(new_surplus_data):
+    """
+    Update surplus worksheet, add new row with the calculated data 
+    """
+    print("updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(new_surplus_data)
+    print("Surplus worksheet updated successfully.\n")
+
+
 def main():
     """ Run all program functions """
     data = get_sales_data()
@@ -84,6 +94,7 @@ def main():
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches Data Automation")
